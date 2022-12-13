02a43942d5f84a39cba9ddd474a3e572f8c4af3f
Matthew-Tangk <118222001+Matthew-Tangk@users.noreply.github.com>
2022-12-13 19:41:04 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,24 @@
 // JavaScript Document
 console.log("hi");
+
+// MENU OPENEN //
+var openButton = document.querySelector("header > button")
+
+openButton.addEventListener("click", openMenu);
+
+function openMenu(){
+    var deNav = document.querySelector("nav");
+    deNav.classList.add("toonMenu");
+
+    
+}
+
+// MENU SLUITEN //
+var sluitButton = document.querySelector("nav button");
+
+sluitButton.addEventListener("click", sluitMenu);
+
+function sluitMenu() {
+    var deNav = document.querySelector("nav");
+    deNav.classList.remove("toonMenu");
+  }
